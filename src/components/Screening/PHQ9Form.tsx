40952d492f65b263b4659c5bac4b53202f3f98a2
Rajae Robinson <rajrobinson18@gmail.com>
2024-02-28 16:16:37 -0500

    feat(screening): add disclaimer

--- src/components/Screening/PHQ9Form.tsx
+++ src/components/Screening/PHQ9Form.tsx
@@ -34,83 +34,89 @@ export default function PHQ9Form() {
 
   return (
     <>
-    <div data-testid="phq9-form" className="my-4 border-4 border-blue-800 p-4">
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        {questions.map((question, index) => (
-          <div key={index} className="space-y-2">
-            <p className="text-sm text-accent">{`Q${
-              index + 1
-            }: ${question} *`}</p>
-            <div className="flex flex-col ml-8">
-              <label htmlFor={`question-${index + 1}-0`}>
-                <input
-                  type="radio"
-                  id={`question-${index + 1}-0`}
-                  name={`question-${index + 1}`}
-                  value="0"
-                  onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  required
-                  disabled={submitted}
-                />
-                &nbsp;Not at all
-              </label>
-              <label htmlFor={`question-${index + 1}-1`}>
-                <input
-                  type="radio"
-                  id={`question-${index + 1}-1`}
-                  name={`question-${index + 1}`}
-                  value="1"
-                  onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  required
-                  disabled={submitted}
-                />
-                &nbsp;Several days
-              </label>
-              <label htmlFor={`question-${index + 1}-2`}>
-                <input
-                  type="radio"
-                  id={`question-${index + 1}-2`}
-                  name={`question-${index + 1}`}
-                  value="2"
-                  onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  required
-                  disabled={submitted}
-                />
-                &nbsp;More than half the days
-              </label>
-              <label htmlFor={`question-${index + 1}-3`}>
-                <input
-                  type="radio"
-                  id={`question-${index + 1}-3`}
-                  name={`question-${index + 1}`}
-                  value="3"
-                  onChange={(e) => handleAnswerChange(index, e.target.value)}
-                  required
-                  disabled={submitted}
-                />
-                &nbsp;Nearly every day
-              </label>
+      <div
+        data-testid="phq9-form"
+        className="my-4 border-4 border-blue-800 p-4"
+      >
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          {questions.map((question, index) => (
+            <div key={index} className="space-y-2">
+              <p className="text-sm text-accent">{`Q${
+                index + 1
+              }: ${question} *`}</p>
+              <div className="flex flex-col ml-8">
+                <label htmlFor={`question-${index + 1}-0`}>
+                  <input
+                    type="radio"
+                    id={`question-${index + 1}-0`}
+                    name={`question-${index + 1}`}
+                    value="0"
+                    onChange={(e) => handleAnswerChange(index, e.target.value)}
+                    required
+                    disabled={submitted}
+                  />
+                  &nbsp;Not at all
+                </label>
+                <label htmlFor={`question-${index + 1}-1`}>
+                  <input
+                    type="radio"
+                    id={`question-${index + 1}-1`}
+                    name={`question-${index + 1}`}
+                    value="1"
+                    onChange={(e) => handleAnswerChange(index, e.target.value)}
+                    required
+                    disabled={submitted}
+                  />
+                  &nbsp;Several days
+                </label>
+                <label htmlFor={`question-${index + 1}-2`}>
+                  <input
+                    type="radio"
+                    id={`question-${index + 1}-2`}
+                    name={`question-${index + 1}`}
+                    value="2"
+                    onChange={(e) => handleAnswerChange(index, e.target.value)}
+                    required
+                    disabled={submitted}
+                  />
+                  &nbsp;More than half the days
+                </label>
+                <label htmlFor={`question-${index + 1}-3`}>
+                  <input
+                    type="radio"
+                    id={`question-${index + 1}-3`}
+                    name={`question-${index + 1}`}
+                    value="3"
+                    onChange={(e) => handleAnswerChange(index, e.target.value)}
+                    required
+                    disabled={submitted}
+                  />
+                  &nbsp;Nearly every day
+                </label>
+              </div>
             </div>
+          ))}
+          <div className="mt-6">
+            {submitted ? (
+              <ResultsModal
+                heading={`Result: ${determineDepressionLevel(
+                  calculateScore(),
+                )}`}
+                body={`The PHQ-9 depression screening tool on this website provides informational guidance only and is not a substitute for professional diagnosis or treatment. Its results should not be considered a definitive assessment; please consult a qualified healthcare professional for an accurate evaluation of your mental health.
+              Based on your responses you received a total score of ${calculateScore()}.`}
+              />
+            ) : (
+              <button
+                type="submit"
+                className="bg-primary text-white px-4 py-2 disabled:bg-gray-500"
+                disabled={submitted}
+              >
+                Submit
+              </button>
+            )}
           </div>
-        ))}
-        <div className="mt-6">
-          {submitted ? (
-            <ResultsModal
-              heading={`Result: ${determineDepressionLevel(calculateScore())}`}
-              body={`Based on your responses you received a total score of ${calculateScore()}.`}
-            />
-          ) : (
-            <button
-              type="submit"
-              className="bg-primary text-white px-4 py-2 disabled:bg-gray-500"
-              disabled={submitted}
-            >
-              Submit
-            </button>
-          )}
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
       <p className="text-accent">
         Questionnaire adapted from&nbsp;
         <a
