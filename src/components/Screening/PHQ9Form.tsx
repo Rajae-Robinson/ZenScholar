4fc8cfe3847998d294ae9a62f992545015730a8c
Rajae Robinson <rajrobinson18@gmail.com>
2024-04-16 07:44:22 -0500

    feat: update results modal

--- src/components/Screening/PHQ9Form.tsx
+++ src/components/Screening/PHQ9Form.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ResultsModal from "./ResultsModal";
 import { PHQ9Questions as questions } from "./constants";
+import { Link } from "react-router-dom";
 
 export default function PHQ9Form() {
   const [answers, setAnswers] = useState(Array(questions.length).fill(0));
@@ -99,11 +100,72 @@ export default function PHQ9Form() {
           <div className="mt-6">
             {submitted ? (
               <ResultsModal
-                heading={`Result: ${determineDepressionLevel(
-                  calculateScore(),
-                )}`}
-                body={`The PHQ-9 depression screening tool on this website provides informational guidance only and is not a substitute for professional diagnosis or treatment. Its results should not be considered a definitive assessment; please consult a qualified healthcare professional for an accurate evaluation of your mental health.
-              Based on your responses you received a total score of ${calculateScore()}.`}
+                heading={`Result`}
+                body={
+                  <p className="leading-relaxed text-gray-500">
+                    The PHQ-9 depression screening tool on this website provides
+                    informational guidance only and is{" "}
+                    <strong>
+                      not a substitute for professional diagnosis or treatment
+                    </strong>
+                    . Its results should not be considered a definitive
+                    assessment; please consult a qualified healthcare
+                    professional for an accurate evaluation of your mental
+                    health.
+                    <br />
+                    <br />
+                    Here is your result:
+                    <br />
+                    <strong>Score</strong>: {calculateScore()}
+                    <br />
+                    <strong>Depression Severity</strong>:{" "}
+                    {determineDepressionLevel(calculateScore())}
+                    <br />
+                    <br />
+                    <strong>
+                      <i>Interpretation</i>
+                    </strong>
+                    : Scores of 5, 10, 15, and 20 indicate the threshold
+                    different levels of depression: mild, moderate, moderately
+                    severe, and severe, respectively. Note: If a patient answers
+                    yes to Question 9 about suicide risk, they should undergo
+                    further evaluation by someone skilled in assessing this
+                    risk.
+                    <br />
+                    <br />
+                    If you are in crisis or need immediate assistance, please{" "}
+                    <Link to="/resources" className="text-accent">
+                      contact a crisis helpline or emergency services in your
+                      area
+                    </Link>
+                    . Take care and be kind to yourself. You are not alone.
+                    <br />
+                    <br />
+                    <Link
+                      to="/resources"
+                      target="_blank"
+                      className="text-accent font-bold"
+                    >
+                      Guidlines for medical professionals on how to interpret
+                      PHQ-9 scores.
+                      <svg
+                        className="w-3 h-3 ms-2.5 inline-block rtl:rotate-[270deg]"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 18"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+                        />
+                      </svg>
+                    </Link>
+                  </p>
+                }
               />
             ) : (
               <button
