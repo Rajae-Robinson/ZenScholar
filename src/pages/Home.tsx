2a3a89fdd56031952c17908d3b08aa968c977532
LasheaBeaton <114885855+LasheaBeaton@users.noreply.github.com>
2024-05-02 21:17:39 -0500

    Starting  footer edits from last year

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,96 @@
+import hero from "../assets/img/hero.svg";
 import bg from "../assets/img/hero-bg.svg";
+import { Link } from "react-router-dom";
+import SectionItem from "../components/Home/SectionItem";
 
+import stressGirl from "../assets/img/stress-girl.svg";
 import happyBoy from "../assets/img/happy-boy.svg";
 import onlineTest from "../assets/img/online-test.svg";
 import psychologist from "../assets/img/psychologist.svg";
-
-import Hero from "../components/Home/Hero";
+import test from "../assets/icons/test.svg";
+import searchResources from "../assets/icons/search-resources.svg";
 import HomeSection from "../components/Home/HomeSection";
-import MainSection from "../components/Home/MainSection";
 
 export default function Home() {
   return (
     <>
       <img
         src={bg}
-        className="absolute inset-0 w-[80%] -z-20 object-cover left-[30%]"
+        className="absolute inset-0 h-screen w-full -z-20 object-cover object-center"
         aria-hidden="true"
       />
-      <Hero />
-      <main className="mt-16 lg:mt-0 space-y-16">
-        <MainSection />
+      <header className="flex h-screen overflow-clip">
+        <div className="h-fit flex">
+          <div>
+            <h1 className="mt-16 text-heading text-3xl font-thin">
+              <span className="font-bold">ZenScholar:</span>
+              Tools & Resources for Student Mental Wellness
+            </h1>
+            <p className="text-body mt-2">
+              Ever felt the weight of university life impacting your mental
+              well-being? <b>You&apos;re not alone</b>. Our mission is to shed
+              light on the silent mental struggles of Jamaican university
+              students. Join us in breaking the stigma and discovering the
+              support you deserve. Take our depression test, explore mental
+              health resources, and take the first step towards a brighter and
+              more balanced campus life.
+            </p>
+            <div className="mt-12 flex space-x-4">
+              <Link to="/screening">
+                <button className="text-white bg-primary py-2 px-4 hover:bg-accent whitespace-nowrap">
+                  Go to Depression Screening Test
+                </button>
+              </Link>
+              <Link to="/resources">
+                <button className="border-primary border-2 py-2 px-4 hover:shadow-md whitespace-nowrap">
+                  Mental Health Resources
+                </button>
+              </Link>
+            </div>
+            <p className="text-xs mt-2">
+              <span className="font-bold">FREE. </span>
+              No sign-up required
+            </p>
+          </div>
+          <img
+            src={hero}
+            className="w-0 sm:w-[200px] md:w-[300px] lg:w-[400px] self-center"
+            alt="girl meditating"
+          />
+        </div>
+      </header>
+      <main className="space-y-16">
+        <section className="flex">
+          <div>
+            <h2 className="text-heading text-lg">
+              Juggling Life and University
+            </h2>
+            <p className="mt-4 text-body">
+              University life is a maze of challenges, from academic demands to
+              personal battles. The pressure cooker of expectations,
+              relationships, finances, and the quest for identity—we understand
+              the whirlwind that young adults navigate. In these moments, mental
+              health often takes a backseat.
+            </p>
+            <div className="flex mt-6">
+              <SectionItem
+                icon={test}
+                title="PHQ-9 Screening Form"
+                description="The PHQ-9 self-assessment is a simple yet powerful tool used to gauge the presence and severity of depression symptoms."
+                linkText="Take Test >>"
+                linkHref="/screening"
+              />
+              <SectionItem
+                icon={searchResources}
+                title="Mental Health Resources"
+                description="Access trusted mental health guidance and effective techniques to manage depression, stress, or anxiety, along with contact details for local mental health support centers."
+                linkText="Search Mental Health Resources >>"
+                linkHref="/resources"
+              />
+            </div>
+          </div>
+          <img src={stressGirl} className="w-[500px]" alt="stressed girl" />
+        </section>
         <HomeSection
           img={happyBoy}
           heading={"Embracing Emotional Well-being"}
@@ -49,7 +121,37 @@ export default function Home() {
           btnText="Mental Health Resources"
           btnHref="/resources"
         />
+
+
+      <footer className="w-full bg-blue-950 text-white absolute bottom-0 inset-x-0 mt-30 pt-30 h-auto">
+        <div className=" w-full  flex justify-around space-y-8 h-auto">
+          
+          <div >
+          </div>
+
+          <div>
+            <ul>
+            <span className="text-white font-bold"> Resources</span>
+          <span className="text-white font-bold"> Resources</span>
+          <span className="text-white font-bold"> Resources</span>
+          <span className="text-white font-bold"> Resources</span>
+            </ul>
+
+          </div>
+
+          <div>
+          <span className="text-white font-bold" ><h4>Form</h4></span>
+          </div>
+
+          <div>
+          <span className="text-white font-bold" ><h4>Other Pages</h4></span>
+          </div>
+        </div>  
+
+      </footer>
       </main>
+
+      
     </>
   );
 }
